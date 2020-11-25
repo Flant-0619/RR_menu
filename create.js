bf3ce1adce871645991cc89a2c1113f5e60ac946
Flant-0619 <Flant1123@gmail.com>
2020-11-25 16:11:58 +0900

    change Object_cnt

--- create.js
+++ create.js
@@ -1,19 +1,13 @@
-var rect = [];
-var circle = []
-var text = []
 var undo = [];
 var redo = [];
-var twitter = [];
-var Twitter_number = 0;
-var Rect_number = 0;
-var Circle_number = 0;
-var Text_number = 0;
 var Object_cnt = 0;
+var Object_list = [];
 var shift = 10;
+
 function CreateRect(){
   undo.push(canvasA.toDatalessJSON());
   redo = [];
-  rect[Rect_number] = new fabric.Rect({
+  Object_list[Object_cnt] = new fabric.Rect({
     id: Object_cnt,
     left: 80,
     top: 80,
@@ -22,14 +16,13 @@ function CreateRect(){
     snapAngle: 5,
     fill: 'orangered',
   });
-  canvasA.add(rect[Rect_number]).setActiveObject(rect[Rect_number]);
-  Rect_number = Rect_number + 1;
+  canvasA.add(Object_list[Object_cnt]).setActiveObject(Object_list[Object_cnt]);
   Object_cnt = Object_cnt + 1;
 }
 function CreateCircle(){
   undo.push(canvasA.toDatalessJSON());
   redo = [];
-  circle[Circle_number] = new fabric.Circle({
+  Object_list[Object_cnt] = new fabric.Circle({
     id: Object_cnt,
     left: 50,
     top: 50,
@@ -38,14 +31,13 @@ function CreateCircle(){
     snapAngle: 5,
     //name : Object_cnt
   });
-  canvasA.add(circle[Circle_number]).setActiveObject(circle[Circle_number]);;
-  Circle_number = Circle_number + 1;
+  canvasA.add(Object_list[Object_cnt]).setActiveObject(Object_list[Object_cnt]);;
   Object_cnt = Object_cnt + 1;
 }
 function CreateText(){
   undo.push(canvasA.toDatalessJSON());
   redo = [];
-  text[Text_number] = new fabric.IText('Mytext', {
+  Object_list[Object_cnt] = new fabric.IText('Mytext', {
     id: Object_cnt,
     width: 150,
     top: 5,
@@ -56,8 +48,7 @@ function CreateText(){
     snapAngle: 5,
     //name : Object_cnt
   });
-  canvasA.add(text[Text_number]).setActiveObject(text[Text_number]);;
-  Text_number = Text_number + 1;
+  canvasA.add(Object_list[Object_cnt]).setActiveObject(Object_list[Object_cnt]);;
   Object_cnt = Object_cnt + 1;
 }
 
@@ -76,11 +67,10 @@ canvasA.on('selection:updated', function(e){
 });
 
 function CreateTwitter(){
-  twitter[Twitter_number] = new fabric.Image.fromURL('twitter.png',function(oImg){
+  Object_list[Object_cnt] = new fabric.Image.fromURL('twitter.png',function(oImg){
     oImg.set({id: Object_cnt, left:80, top:80, snapAngle: 5});
     oImg.scale(0.1);
     canvasA.add(oImg).setActiveObject(oImg);
   });
-  Twitter_number = Twitter_number + 1;
   Object_cnt = Object_cnt + 1;
 }
